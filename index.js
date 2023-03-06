e56211a625709158e934879598a7615e927e68e3
DamilolaAbiona <99470227+DamilolaAbiona@users.noreply.github.com>
2023-03-06 17:29:04 +0100

    ValueTimesIndex Added

--- index.js
+++ index.js
@@ -73,3 +73,13 @@ if ( arr[i] > largestNum )
 return largestNum
 }
 console.log(findLargestNum(arr))
+// 5 Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array:
+// e.g.,
+//  valTimesIndex([1,2,3]) // [0,2,6]
+// 		valTimesIndex([5,10,15]) // [0,10,30]
+function valTimesIndex(arr){
+      return arr.map(function(value,index,array){
+		return value*index;
+	});   
+}
+console.log(valTimesIndex([5, 0, 10, 3, 2]))
